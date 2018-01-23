34da93da4bbae2419f712e8b31ebb0adfb79d685
mohitkumaris <mohitkumaris@gmail.com>
2018-01-23 22:10:03 +0530

    Mongo Db server by mlab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,11 @@
 const mongoose=require('mongoose');
 
-mongoose.Promise=global.Promise;
+var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+    replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };
 
-mongoose.connect('mongodb://192.168.108.181:27017/ToDoApp');
+mongoose.Promise=global.Promise;
+//mongodb://192.168.108.181:27017/ToDoApp
+mongoose.connect('mongodb://mohitkumaris:fun974dose366@ds111638.mlab.com:11638/todos',options);
 
 module.exports={
     mongoose
