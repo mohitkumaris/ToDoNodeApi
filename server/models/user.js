5a681443f308715245e6142ab9c47343c0bbc534
mohitkumaris <mohitkumaris@gmail.com>
2018-01-24 22:19:41 +0530

    Override toJSON method

--- server/models/user.js
+++ server/models/user.js
@@ -1,68 +1,62 @@
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
 const validator = require('validator');
-const jwt=require('jsonwebtoken');
-const _=require('lodash');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-
-var UserSchema= new mongoose.Schema({
-    email:{
-        type:String,
-        required:true,
-        minlength:1,
-        trim:true,
-        unique:true,
-        validate:{
-            validator:validator.isEmail,
-            message:'{VALUE} is not a valid email'
+var UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 1,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid email'
         }
     },
-    password:{
-        type:String,
-        required:true,
-        minlength:6
+    password: {
+        type: String,
+        require: true,
+        minlength: 6
     },
-    tokens:[{
-        access:{
-           type:String,
-           required:true
+    tokens: [{
+        access: {
+            type: String,
+            required: true
         },
-        token:{
-            type:String,
-            required:true
+        token: {
+            type: String,
+            required: true
         }
-
     }]
 });
-
 /*
-Instance methods
+ It's automatically called when we respond to
+ the express request with res.send.
+ That converts our object to a string by calling JSON.stringify.
+ JSON.stringify is what calls toJSON. Here's an isolated example:
+ It Overrides built in toJSON
  */
-// coz arrow function can't access this
+UserSchema.methods.toJSON = function () {
+    var user = this;
+    var userObject = user.toObject();
 
-UserSchema.methods.toJson=function () {
+    return _.pick(userObject, ['_id', 'email']);
+};
 
-    var user=this;
-    var UserObject=user.toObject();
+UserSchema.methods.generateAuthToken = function () {
+    var user = this;
+    var access = 'auth';
+    var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
 
-    return _.pick(UserObject,['_id','email']);
-}
+    user.tokens.push({access, token});
 
-UserSchema.methods.generateTokens=function () {
-    var user=this;
-    var access="auth";
-    var token=jwt.sign({_id:user._id.toHexString(),access},'abc123').toString();
-
-    user.tokens.push({access,token});
-
-    return user.save().then(()=>{
-
+    return user.save().then(() => {
         return token;
     });
-
-}
-
-var User=mongoose.model('User',UserSchema);
-
-module.exports={
-    User
 };
+
+var User = mongoose.model('User', UserSchema);
+
+module.exports = {User}
